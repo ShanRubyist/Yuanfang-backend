c5c9c4ae4525990bf316e7276c8d1e615985473c
shan <2302531309@qq.com>
2023-06-03 22:51:53 +0800

    FEAT: 集成 devise_token_auth 和dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # root "users/sessions#new"
+
+  namespace :api, path: nil do
+    namespace :v1 do
+      # match "*path", to: "api#gone", via: :all
+    end
+  end
 end

f4df2cfa09689d0920b212a662d2347c197d3ee3
shan <2302531309@qq.com>
2023-06-10 21:57:11 +0800

    FEAT: 使用 Chat Completions 接口

--- app/controllers/api/v1/completions_controller.rb
+++ app/controllers/api/v1/completions_controller.rb
@@ -6,21 +6,25 @@ class Api::V1::CompletionsController < ApplicationController
   attr_reader :client
 
   def achieve
-    raise 'prompt can not be empty' unless params['prompt'].present?
+    raise 'content can not be empty' unless params['content'].present?
 
-    model = "text-davinci-003"
-    prompt = params['prompt']
-    max_tokens = 4096
-    response = client.completions(
-      parameters: {
-        model: model,
-        prompt: prompt,
-        max_tokens: max_tokens
-      })
-
-    render json: {
-      answer: (response["choices"].map { |c| c["text"] }).to_json
-    }
+    begin
+      response = client.chat(
+        parameters: {
+          model: "gpt-3.5-turbo",
+          messages: [
+            { "role": "system", "content": "假设你是个产品设计师，需要根据用户描述给出设计方案、功能提示文案和免责文案" },
+            { "role": "system", "content": "以markdown格式返回" },
+            { "role": "user", "content": params['content'] }
+          ],
+          temperature: 0.1
+        })
+      render json: {
+        answer: response.dig("choices", 0, "message", "content")
+      }
+    rescue => e
+      render json: e.to_json, status: 500
+    end
   end
 
   private
@@ -29,10 +33,10 @@ class Api::V1::CompletionsController < ApplicationController
     OpenAI.configure do |config|
       config.access_token = ENV.fetch("OPENAI_API_KEY")
       config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") rescue nil
-      config.uri_base = ENV.fetch("OPENAI_API_BASE_URL") { 'https://api.openai.com/'}   # 必须http[s] 开头，/ 结尾
+      config.uri_base = ENV.fetch("OPENAI_API_BASE_URL") { 'https://api.openai.com/' } # 必须http[s] 开头，/ 结尾
       config.request_timeout = 240 # Optional
     end
 
-    @client = OpenAI::Client.new
+    @client ||= OpenAI::Client.new
   end
 end
